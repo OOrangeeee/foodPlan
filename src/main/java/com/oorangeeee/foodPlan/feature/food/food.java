2811e7cbe12fcce5fd1d8b93e0dca135d32dfaad
晋晨曦 <Jin0714@outlook.com>
2024-06-11 11:30:16 +0800

    feat(food): add the interface of food

---
+++ src/main/java/com/oorangeeee/foodPlan/feature/food/food.java
@@ -0,0 +1,34 @@
+package com.oorangeeee.foodPlan.feature.food;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 晋晨曦
+ */
+public interface food {
+    // 一个接口，定义了食物的属性
+    // 1. 食物的名字
+    // 2. 早午晚餐，下午茶，零食，加餐（多选）
+    // 3. 食物的类型: 面食，蒸点，炒菜，汤，饮料，水果，火锅，烧烤，快餐，轻食，甜品，蔬菜，油炸食品，烘焙，米饭，零食（多选）
+    // 4. 备注
+    // food中储存食物的时间类型和类型只存一个由,分隔的字符串，如"早餐,午餐"，"面食,蒸点"
+    // 返回的时候再分割成数组
+    // 同理在set的时候也是接受一个字符串
+    // 此类必须实现序列化接口，以便于存储
+    String[] timeType = {"早餐", "午餐", "晚餐", "下午茶", "零食", "加餐"};
+    String[] foodType = {"面食", "蒸点", "炒菜", "汤", "饮料", "水果", "火锅", "烧烤", "快餐", "轻食", "甜品", "蔬菜", "油炸食品", "烘焙", "米饭", "零食"};
+
+    // getter and setter
+
+    String getFoodName();
+    void setFoodName(String foodName);
+    String[] getTimeType();
+    void setTimeType(String timeType);
+    String[] getFoodType();
+    void setFoodType(String foodType);
+    String getRemark();
+    void setRemark(String remark);
+
+}
